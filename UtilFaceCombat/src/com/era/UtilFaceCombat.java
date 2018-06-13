5257aef2aa4b60cd1cc0021c3f90e50c5a930949
era <edvaldoer4@gmail.com>
2018-06-13 18:05:35 -0300

    Projeto do Tomus

---
+++ UtilFaceCombat/src/com/era/UtilFaceCombat.java
@@ -0,0 +1,36 @@
+package com.era;
+
+public class UtilFaceCombat {
+
+	public static void main(String[] args) {
+		
+		Lutador l[] = new Lutador[6];
+		
+		
+		
+		l[0] = new Lutador("Fransuar", "França", 31, 1.75f,
+				68.9f, 11, 2, 1);
+		l[1] = new Lutador("Chico Brasileiro", "Brasil", 29, 1.68f,
+				57.8f, 14, 2, 3);
+		l[2] = new Lutador("Jon João", "EUA", 35, 1.65f,
+				80.9f, 12, 2, 1);
+		l[3] = new Lutador("Australindo", "Austrália", 28, 1.93f,
+				81.5f, 13, 0, 2);
+		l[4] = new Lutador("Tonho", "Brasil", 37, 1.81f, 
+				119.3f, 5, 4, 3);
+		l[5] = new Lutador("Explojion", "EUA", 30, 1.84f,
+				105.7f, 12, 2, 4);
+		
+		
+		//l[5].empatarLuta();
+		//l[5].status();
+		
+		Luta UEC01 = new Luta();
+		
+		UEC01.marcarLuta(l[0], l[1]);
+		UEC01.lutar();
+		l[0].status();
+		l[1].status();
+	}
+
+}
